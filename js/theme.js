4ed30bb1e20573abba9c426bd2d9e2115b915316
by-aza <by.azazica@gmail.com>
2026-04-13 15:31:20 +0300

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -4,9 +4,11 @@ document.getElementById("themeToggle").onclick = function () {
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
+        document.body.classList.remove("light-mode");
         localStorage.setItem("theme", "dark");
         this.textContent = "☀️";
     } else {
+        document.body.classList.add("light-mode");
         localStorage.setItem("theme", "light");
         this.textContent = "🌙";
     }
@@ -16,4 +18,7 @@ document.getElementById("themeToggle").onclick = function () {
 if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark-mode");
     document.getElementById("themeToggle").textContent = "☀️";
+} else {
+    document.body.classList.add("light-mode");
+    document.getElementById("themeToggle").textContent = "🌙";
 }
